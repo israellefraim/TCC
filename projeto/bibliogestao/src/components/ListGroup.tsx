060c65b9beb4094e0ffaf33de0b948c9783a38f2
israellefraim <israell.efraim@gmail.com>
2026-08-19 16:32:40 -0300

    Realiza testes do react

--- projeto/bibliogestao/src/components/ListGroup.tsx
+++ projeto/bibliogestao/src/components/ListGroup.tsx
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 function ListGroup() {
   const movies = [
     "How To Train Your Dragon I",
@@ -6,13 +8,22 @@ function ListGroup() {
     "Kill Bill Vol. II",
     "Mônica's Ganga",
   ];
+  const [selectedIndex, setSelectedIndex] = useState(-1);
 
   return (
     <>
       <h1>List</h1>
       <ul className="list-group">
-        {movies.map((movie) => (
-          <li key={movie} className="list-group-item">
+        {movies.map((movie, index) => (
+          <li
+            className={
+              selectedIndex == index
+                ? "list-group-item active"
+                : "list-group-item"
+            }
+            key={movie}
+            onClick={() => setSelectedIndex(index)}
+          >
             {movie}
           </li>
         ))}
