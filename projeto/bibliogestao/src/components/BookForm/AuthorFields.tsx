3becf802827b02b57f487eee505f7111e1ba8862
Luís Marinho <luisdm5309@gmail.com>
2026-09-09 14:22:59 -0300

    Separa os campos de Editora

--- projeto/bibliogestao/src/components/BookForm/AuthorFields.tsx
+++ projeto/bibliogestao/src/components/BookForm/AuthorFields.tsx
@@ -1,7 +1,4 @@
-function AuthorFields ({
-
-}) 
-{    
+function AuthorFields ({}) {    
     return (
         <>
         {/* AUTHOR FIELD */}
--- projeto/bibliogestao/src/components/BookForm/BookForm.tsx
+++ projeto/bibliogestao/src/components/BookForm/BookForm.tsx
@@ -3,6 +3,7 @@ import { supabase } from "../../lib/supabase-client.ts";
 
 import BookFields from "./BookFields.tsx";
 import AuthorFields from "./AuthorFields.tsx";
+import PublisherFields from "./PublisherFields.tsx";
 
 function BookForm() {
     const [isbn, setISBN] = useState("");
@@ -75,9 +76,14 @@ function BookForm() {
         <AuthorFields />
 
         {/* PUBLISHERS FIELD */}
-        <fieldset className="border border-dark p-3 rounded mb-3">
-          <legend className="float-none w-auto px-2">Editora</legend>
-        </fieldset>
+        <PublisherFields 
+          publisherName={publisherName}
+          setPublisherName={setPublisherName}
+          publisherCNPJ={publisherCNPJ}
+          setPublisherCNPJ={setPublisherCNPJ}
+          publisherCountry={publisherCountry}
+          setPublisherCountry={setPublisherCountry}
+        />
 
         {/* COLLECTION FIELD */}
         <fieldset className="border border-dark p-3 rounded mb-3">
